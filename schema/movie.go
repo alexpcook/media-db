bcb82a047e1401ab3a31c7254296aa6c5f0a264d
Alex Cook <apcook7104@gmail.com>
2021-03-15 02:13:45 -0700

    Add movie constructor implementation

--- schema/movie.go
+++ schema/movie.go
@@ -1,14 +1,42 @@
 package schema
 
-import "time"
+import (
+	"fmt"
+	"strings"
+)
 
+// Movie contains information about a single film. DateWatched is a Unix timestamp.
 type Movie struct {
-	Title       string    `json:"title"`
-	Director    string    `json:"director"`
-	YearMade    int       `json:"year"`
-	DateWatched time.Time `json:"date"`
+	Title       string `json:"title"`
+	Director    string `json:"director"`
+	YearMade    int    `json:"year"`
+	DateWatched int64  `json:"date"`
 }
 
+// NewMovie validates the fields of a Movie struct, creates it, and returns a pointer.
+// If there are validation problems, a non-nil error is returned.
 func NewMovie(title, director string, yearMade int, dateWatched string) (*Movie, error) {
-	return &Movie{}, nil
+	if strings.TrimSpace(title) == "" {
+		return nil, fmt.Errorf("title cannot be only space, got %q", title)
+	}
+
+	if strings.TrimSpace(director) == "" {
+		return nil, fmt.Errorf("director cannot be only space, got %q", director)
+	}
+
+	if yearMade < 1 {
+		return nil, fmt.Errorf("yearMade must be positive, got %d", yearMade)
+	}
+
+	unixTime, err := StringToUnixTime(dateWatched)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Movie{
+		Title:       title,
+		Director:    director,
+		YearMade:    yearMade,
+		DateWatched: unixTime,
+	}, nil
 }
