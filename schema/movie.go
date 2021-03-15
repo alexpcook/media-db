9cc399ad11ef9eaab2fc2e2a3ec2318fb81e57e0
Alex Cook <apcook7104@gmail.com>
2021-03-14 21:46:37 -0700

    Make date watched a pointer to time.Time

--- schema/movie.go
+++ schema/movie.go
@@ -3,8 +3,8 @@ package schema
 import "time"
 
 type Movie struct {
-	Title       string    `json:"title"`
-	Director    string    `json:"director"`
-	YearMade    int       `json:"year"`
-	DateWatched time.Time `json:"date"`
+	Title       string     `json:"title"`
+	Director    string     `json:"director"`
+	YearMade    int        `json:"year"`
+	DateWatched *time.Time `json:"date"`
 }
