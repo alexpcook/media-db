a04c66e51080794975debb517272e9b1c390f0c7
Alex Cook <apcook7104@gmail.com>
2021-03-14 20:21:01 -0700

    Add json structure for types

--- schema/movie.go
+++ schema/movie.go
@@ -1,1 +1,10 @@
 package schema
+
+import "time"
+
+type Movie struct {
+	Title       string    `json:"title"`
+	Director    string    `json:"director"`
+	YearMade    int       `json:"year"`
+	DateWatched time.Time `json:"date"`
+}
--- schema/music.go
+++ schema/music.go
@@ -1,1 +1,10 @@
 package schema
+
+import "time"
+
+type Music struct {
+	Title        string    `json:"title"`
+	Artist       string    `json:"artist"`
+	YearMade     int       `json:"year"`
+	DateListened time.Time `json:"date"`
+}
