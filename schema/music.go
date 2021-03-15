00971616a707b6aa20cf57e7c6de302d3134bfc7
Alex Cook <apcook7104@gmail.com>
2021-03-15 03:56:18 -0700

    Convert DateListened to Unix timestamp

--- schema/music.go
+++ schema/music.go
@@ -1,12 +1,10 @@
 package schema
 
-import "time"
-
 type Music struct {
-	Title        string    `json:"title"`
-	Artist       string    `json:"artist"`
-	YearMade     int       `json:"year"`
-	DateListened time.Time `json:"date"`
+	Title        string `json:"title"`
+	Artist       string `json:"artist"`
+	YearMade     int    `json:"year"`
+	DateListened int64  `json:"date"`
 }
 
 func NewMusic(title, artist string, yearMade int, dateListened string) (*Music, error) {
