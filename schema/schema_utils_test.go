c201afcf49432c1ba9aa9c68b762e39c4a7de13b
Alex Cook <apcook7104@gmail.com>
2021-03-15 02:50:54 -0700

    Fix misc testing issues

--- schema/schema_utils_test.go
+++ schema/schema_utils_test.go
@@ -11,12 +11,12 @@ func TestStringToUnixTime(tt *testing.T) {
 		input   string
 		isError bool
 	}{
-		{"2006-01-02T15:04:05Z07:00", false},
-		{"2006-01-02", false},
-		{"0036-01-02T15:04:05Z07:00", false},
-		{"2021-10-30T00:15:15.65Z-08:00", false},
+		{"2006-01-02T15:04:05Z", false},
+		{"2006-01-02T00:00:00-07:00", false},
+		{"1900-01-02T15:04:05+07:00", false},
+		{"2021-10-30T00:15:15.65Z", false},
 		{"invalid date string", true},
-		{"2021-13-11", true},
+		{"2021-11-11", true},
 	}
 
 	unixTimeZero := time.Unix(0, 0)
@@ -24,11 +24,16 @@ func TestStringToUnixTime(tt *testing.T) {
 		tt.Run(fmt.Sprintf("case-%d", i), func(subtt *testing.T) {
 			got, err := StringToUnixTime(test.input)
 
-			if test.isError && err == nil {
-				subtt.Fatal("want error, got nil")
+			if test.isError {
+				if err == nil {
+					subtt.Fatal("want error, got nil")
+				}
+				return
+			} else if err != nil {
+				subtt.Fatal(err)
 			}
 
-			if diff := time.Unix(got, 0).Sub(unixTimeZero); diff != time.Duration(got) {
+			if diff := time.Unix(got, 0).Sub(unixTimeZero) / 1e9; diff != time.Duration(got) {
 				subtt.Fatalf("want zero diff, got %v", diff)
 			}
 		})
