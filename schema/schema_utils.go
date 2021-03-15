c1866e52df2a3e5b0ccf027ecd3b68d5c2f519f5
Alex Cook <apcook7104@gmail.com>
2021-03-15 03:43:40 -0700

    year must be greater than 1850

--- schema/schema_utils.go
+++ schema/schema_utils.go
@@ -1,17 +1,43 @@
 package schema
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
-// StringToUnixTime converts a string s that meets the specifications
-// of RFC 3339 to a Unix timestamp. If the string cannot be parsed
-// according to RFC 3339, a non-nil parsing error is returned.
+// StringToUnixTime converts a string s of format 'yyyy-mm-dd' to a Unix timestamp.
+// The time of the returned value is always 00:00:00 UTC.
+// If the string cannot be parsed to a valid date, a non-nil error is returned.
 func StringToUnixTime(s string) (int64, error) {
-	t, err := time.Parse(time.RFC3339, s)
-	if err != nil {
-		return 0, err
+	strFields := strings.SplitN(s, "-", 3)
+	if numFields := len(strFields); numFields != 3 {
+		return 0, fmt.Errorf("length of date string is not 3, got %d", numFields)
 	}
 
-	return t.Unix(), nil
+	intFields := make([]int, 3)
+	var err error
+	for i := range intFields {
+		intFields[i], err = strconv.Atoi(strFields[i])
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	year, month, day := intFields[0], intFields[1], intFields[2]
+
+	if year < 1850 {
+		return 0, fmt.Errorf("year must be greater than 1850, got %d", year)
+	}
+
+	if month < 1 || month > 12 {
+		return 0, fmt.Errorf("month must be between 1 and 12, got %d", month)
+	}
+
+	if day < 1 || day > 31 {
+		return 0, fmt.Errorf("day must be between 0 and 31, got %d", day)
+	}
+
+	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC).Unix(), nil
 }
