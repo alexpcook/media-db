5f24bf65a5455ffed129c7e380776a667c4ea439
Alex Cook <apcook7104@gmail.com>
2021-03-15 04:07:44 -0700

    Refactor testing functions

--- schema/music_test.go
+++ schema/music_test.go
@@ -1,63 +1,18 @@
 package schema
 
-import (
-	"encoding/json"
-	"fmt"
-	"testing"
-	"time"
-)
+import "testing"
 
-func TestMusicJsonMarshal(t *testing.T) {
-	testTime, err := time.Parse(time.RFC3339, "2008-10-29T01:56:19.32-01:00")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	musicStruct := Music{
-		Title:        "An Album Title",
-		Artist:       "An Artist",
-		YearMade:     1993,
-		DateListened: testTime,
-	}
-
-	_, err = json.Marshal(musicStruct)
-	if err != nil {
-		t.Fatal(err)
-	}
+type newMusicInput struct {
+	title  string
+	artist string
+	year   int
+	date   string
 }
 
-func TestMusicJsonUnmarshal(t *testing.T) {
-	title := "An Album Title"
-	artist := "An Artist"
-	year := 1983
-	date := "2008-10-29T01:56:19.32-01:00" // string must conform to RFC 3339
+type newMusicOutput struct {
+	music   *Music
+	isError bool
+}
 
-	musicBytes := []byte(fmt.Sprintf(`{
-		"title": "%s",
-		"artist": "%s",
-		"year": %d,
-		"date": "%s"
-	}`, title, artist, year, date))
-	musicStruct := Music{}
-
-	err := json.Unmarshal(musicBytes, &musicStruct)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if musicStruct.Title != title {
-		t.Fatalf("Expected %s for Music.Title field, got %s", title, musicStruct.Title)
-	}
-
-	if musicStruct.Artist != artist {
-		t.Fatalf("Expected %s for Music.Artist field, got %s", artist, musicStruct.Artist)
-	}
-
-	if musicStruct.YearMade != year {
-		t.Fatalf("Expected %d for Music.YearMade field, got %d", year, musicStruct.YearMade)
-	}
-
-	if d, err := time.Parse(time.RFC3339, date); !d.Equal(musicStruct.DateListened) || err != nil {
-		t.Fatalf("Expected %s for Music.DateListened field, got %s %v", date, musicStruct.DateListened, err)
-	}
+func TestNewMusic(tt *testing.T) {
 }
