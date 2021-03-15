b7a370d7d2005047c8bd1b9f2d3af32fbf414798
Alex Cook <apcook7104@gmail.com>
2021-03-15 04:33:23 -0700

    Add TestNewMusic()

--- schema/music_test.go
+++ schema/music_test.go
@@ -1,6 +1,9 @@
 package schema
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type newMusicInput struct {
 	title  string
@@ -15,4 +18,44 @@ type newMusicOutput struct {
 }
 
 func TestNewMusic(tt *testing.T) {
+	testCases := []struct {
+		name   string
+		input  newMusicInput
+		output newMusicOutput
+	}{
+		{
+			"basic",
+			newMusicInput{"a title", "an artist", 2000, "2021-03-14"},
+			newMusicOutput{&Music{}, false},
+		},
+	}
+
+	for _, test := range testCases {
+		tt.Run(test.name, func(subtt *testing.T) {
+			music, err := NewMusic(test.input.title, test.input.artist, test.input.year, test.input.date)
+
+			if test.output.isError {
+				if err == nil {
+					subtt.Fatal("want error, got nil")
+				}
+				return
+			} else if err != nil {
+				subtt.Fatal(err)
+			}
+
+			t, err := StringToUnixTime(test.input.date)
+			if err != nil {
+				subtt.Fatal(err)
+			}
+
+			test.output.music.Title = test.input.title
+			test.output.music.Artist = test.input.artist
+			test.output.music.YearMade = test.input.year
+			test.output.music.DateListened = t
+
+			if !reflect.DeepEqual(test.output.music, music) {
+				subtt.Fatalf("want %v, got %v", test.output.music, music)
+			}
+		})
+	}
 }
