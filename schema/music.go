bab06dc6ef87c52e95af20b9802b3a54898a0b0f
Alex Cook <apcook7104@gmail.com>
2021-03-15 03:59:00 -0700

    Add doc strings to exported members

--- schema/music.go
+++ schema/music.go
@@ -1,5 +1,6 @@
 package schema
 
+// Music contains information about a single piece of music. DateListened is a Unix timestamp.
 type Music struct {
 	Title        string `json:"title"`
 	Artist       string `json:"artist"`
@@ -7,6 +8,9 @@ type Music struct {
 	DateListened int64  `json:"date"`
 }
 
+// NewMusic validates the fields of a Music struct, creates it, and returns a pointer.
+// The dateListened parameter should be in the format 'yyyy-mm-dd'.
+// If there are validation problems, a non-nil error is returned.
 func NewMusic(title, artist string, yearMade int, dateListened string) (*Music, error) {
 	return &Music{}, nil
 }
