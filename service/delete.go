817ea0670d3cdb90645313ee388f93385eab6906
Alex Cook <apcook7104@gmail.com>
2021-03-17 16:57:07 -0700

    Add delete object service

--- service/delete.go
+++ service/delete.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"context"
+
+	"github.com/alexpcook/media-db-console/schema"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func (cl *MediaDbClient) Delete(media schema.Media) error {
+	key := media.S3Key()
+
+	_, err := cl.s3Client.DeleteObject(context.TODO(), &s3.DeleteObjectInput{
+		Key: &key,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
