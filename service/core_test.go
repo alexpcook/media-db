b1f8b235b607044ada286388642a0ad46d615a1b
Alex Cook <apcook7104@gmail.com>
2021-03-17 14:37:42 -0700

    Add command line flag for test db config file

--- service/core_test.go
+++ service/core_test.go
@@ -1,68 +1,48 @@
 package service
 
 import (
-	"os"
+	"flag"
 	"testing"
 
 	"github.com/alexpcook/media-db-console/config"
 )
 
-func getLocalEnvVar(s string, tt *testing.T) string {
-	val, isSet := os.LookupEnv(s)
-	if !isSet {
-		tt.Fatalf("env variable %q not set", s)
-	}
-
-	return val
-}
+var (
+	testConfigFilepath *string = flag.String("f", "", "The media db config file for testing")
+)
 
 func TestNewMediaDbClient(tt *testing.T) {
+	if *testConfigFilepath == "" {
+		tt.Fatalf("a media db config file for testing must be supplied, got %q", *testConfigFilepath)
+	}
+
+	cfg, err := config.LoadMediaDbConfig(*testConfigFilepath)
+	if err != nil {
+		tt.Fatal(err)
+	}
+
 	testCases := []struct {
-		name          string
-		mediaDbConfig *config.MediaDbConfig
-		isError       bool
+		name    string
+		isError bool
 	}{
-		{
-			"basic",
-			&config.MediaDbConfig{
-				AWSProfile: getLocalEnvVar("MEDIA_DB_TEST_AWS_PROFILE", tt),
-				AWSRegion:  getLocalEnvVar("MEDIA_DB_TEST_AWS_REGION", tt),
-				S3Bucket:   getLocalEnvVar("MEDIA_DB_TEST_S3_BUCKET", tt),
-			},
-			false,
-		},
-		{
-			"invalid-profile",
-			&config.MediaDbConfig{
-				AWSProfile: "not a profile",
-				AWSRegion:  getLocalEnvVar("MEDIA_DB_TEST_AWS_REGION", tt),
-				S3Bucket:   getLocalEnvVar("MEDIA_DB_TEST_S3_BUCKET", tt),
-			},
-			true,
-		},
-		{
-			"invalid-region",
-			&config.MediaDbConfig{
-				AWSProfile: getLocalEnvVar("MEDIA_DB_TEST_AWS_PROFILE", tt),
-				AWSRegion:  "not a region",
-				S3Bucket:   getLocalEnvVar("MEDIA_DB_TEST_S3_BUCKET", tt),
-			},
-			true,
-		},
-		{
-			"invalid-bucket",
-			&config.MediaDbConfig{
-				AWSProfile: getLocalEnvVar("MEDIA_DB_TEST_AWS_PROFILE", tt),
-				AWSRegion:  getLocalEnvVar("MEDIA_DB_TEST_AWS_REGION", tt),
-				S3Bucket:   "not a bucket",
-			},
-			true,
-		},
+		{"basic", false},
+		{"invalid-profile", true},
+		{"invalid-region", true},
+		{"invalid-bucket", true},
 	}
 
 	for _, test := range testCases {
 		tt.Run(test.name, func(subtt *testing.T) {
-			client, err := NewMediaDbClient(test.mediaDbConfig)
+			switch test.name {
+			case "invalid-profile":
+				cfg.AWSProfile = "not-an-aws-profile"
+			case "invalid-region":
+				cfg.AWSRegion = "not-an-aws-region"
+			case "invalid-bucket":
+				cfg.S3Bucket = "not-an-s3-bucket-ajfkldfjadslfdsflkdsajfdsk"
+			}
+
+			client, err := NewMediaDbClient(cfg)
 
 			if test.isError {
 				if err == nil {
@@ -73,8 +53,8 @@ func TestNewMediaDbClient(tt *testing.T) {
 				subtt.Fatal(err)
 			}
 
-			if test.mediaDbConfig.S3Bucket != client.s3Bucket {
-				subtt.Fatalf("wrong s3 bucket name: want %s, got %s", test.mediaDbConfig.S3Bucket, client.s3Bucket)
+			if cfg.S3Bucket != client.s3Bucket {
+				subtt.Fatalf("wrong s3 bucket name: want %s, got %s", cfg.S3Bucket, client.s3Bucket)
 			}
 		})
 	}
