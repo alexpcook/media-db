66c87bab729aa627e165a22a0abf43884f380a5f
Alex Cook <apcook7104@gmail.com>
2021-03-17 16:42:42 -0700

    Add view service and unit test

--- service/view.go
+++ service/view.go
@@ -1,1 +1,66 @@
 package service
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"strings"
+
+	"github.com/alexpcook/media-db-console/schema"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+// View retrieves media entries from the S3 bucket with keys that match
+// the specified filter.
+func (cl *MediaDbClient) View(filter string) ([]schema.Media, error) {
+	listObjsRes, err := cl.s3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: &cl.s3Bucket,
+		Prefix: &filter,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	mediaRes := make([]schema.Media, 0)
+
+	for _, obj := range listObjsRes.Contents {
+		getObjsRes, err := cl.s3Client.GetObject(context.TODO(), &s3.GetObjectInput{
+			Bucket: &cl.s3Bucket,
+			Key:    obj.Key,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		b := make([]byte, 8)
+		jsonData := make([]byte, 0)
+		for {
+			n, err := getObjsRes.Body.Read(b)
+			jsonData = append(jsonData, b[:n]...)
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return nil, err
+			}
+		}
+
+		switch strings.Split(*obj.Key, "/")[0] {
+		case "movie":
+			media := schema.Movie{}
+			err = json.Unmarshal(jsonData, &media)
+			if err != nil {
+				return nil, err
+			}
+			mediaRes = append(mediaRes, media)
+		case "music":
+			media := schema.Music{}
+			err = json.Unmarshal(jsonData, &media)
+			if err != nil {
+				return nil, err
+			}
+			mediaRes = append(mediaRes, media)
+		}
+	}
+
+	return mediaRes, nil
+}
---
+++ service/view_test.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/alexpcook/media-db-console/config"
+)
+
+func TestView(tt *testing.T) {
+	if *testConfigFilepath == "" {
+		tt.Fatalf("a media db config file for testing must be supplied, got %q", *testConfigFilepath)
+	}
+
+	cfg, err := config.LoadMediaDbConfig(*testConfigFilepath)
+	handleError(err, tt)
+
+	client, err := NewMediaDbClient(cfg)
+	handleError(err, tt)
+
+	res, err := client.View("")
+	handleError(err, tt)
+
+	if len(res) != 2 {
+		tt.Fatalf("expected two entries in response, got %d", len(res))
+	}
+}
