131585ea51a9a3f97ee9992d9f9571c0ac86c5a7
Alex Cook <apcook7104@gmail.com>
2021-03-18 18:46:57 -0700

    Update Key() doc string

--- schema/interface.go
+++ schema/interface.go
@@ -3,6 +3,6 @@ package schema
 // Media is an interface that encompassses methods
 // common to all media types in the database.
 type Media interface {
-	// The unique object key for storage in the database.
+	// Key returns the unique object string for storage in the database.
 	Key() string
 }
