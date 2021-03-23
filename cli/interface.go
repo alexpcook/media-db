9ef95a862cbbe467146244b1684050005d4d948e
Alex Cook <apcook7104@gmail.com>
2021-03-22 17:17:49 -0700

    Add doc strings

--- cli/interface.go
+++ cli/interface.go
@@ -4,10 +4,15 @@ import (
 	"errors"
 )
 
+// MediaDbCommand defines methods common to all CLI commands.
 type MediaDbCommand interface {
+	// Run executes the CLI command and returns any errors encountered during execution.
 	Run() error
 }
 
+// NewMediaDbCommand parses args from the command line and returns the appropriate
+// command for execution. The error will be non-nil if the args do not constitute
+// a valid command.
 func NewMediaDbCommand(args []string) (MediaDbCommand, error) {
 	switch cmd := args[0]; cmd {
 	case SetupCmdName():
