e0f490a511855916bc4fbbec22889cd0554dc591
Alex Cook <apcook7104@gmail.com>
2021-03-22 17:21:22 -0700

    Add doc strings

--- cli/entrypoint.go
+++ cli/entrypoint.go
@@ -10,13 +10,20 @@ import (
 )
 
 var (
+	// MediaDbConfig is the media db configuration for the CLI.
 	MediaDbConfig *config.MediaDbConfig
+	// MediaDbClient is the media db service client for the CLI.
 	MediaDbClient *service.MediaDbClient
 
+	// StdoutLogger is the standard output logger for the CLI.
 	StdoutLogger *log.Logger = log.New(os.Stdout, "", 0)
+	// StderrLogger is the standard error logger for the CLI.
 	StderrLogger *log.Logger = log.New(os.Stderr, "", 0)
 )
 
+// InitDb loads the media database configuration and initializes
+// a service client to communicate with the database. It will return
+// a non-nil error if any of these steps fail.
 func InitDb() {
 	MediaDbConfig, err := config.LoadMediaDbConfig()
 	if err != nil {
@@ -33,6 +40,7 @@ run 'mdb setup' to fix the configuration issue`, err.Error()))
 	}
 }
 
+// Execute is the main entrypoint for callers.
 func Execute() {
 	if len(os.Args) < 2 {
 		StderrLogger.Fatal(GetCLIHelpText())
